3122dc630e5fe1fd42aea48744b2431e8dcdda53
MR-WoLf-ux <zakersamuraee3@gmail.com>
2026-09-19 00:03:07 +0330

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -954,15 +954,15 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
-// if ("serviceWorker" in navigator) {
-//     window.addEventListener("load", () => {
-//         navigator.serviceWorker
-//             .register("/service-worker.js")
-//             .then((registration) => {
-//                 console.log("Service Worker registered:", registration);
-//             })
-//             .catch((error) => {
-//                 console.error("Service Worker registration failed:", error);
-//             });
-//     });
-// }
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("./service-worker.js")
+            .then((registration) => {
+                console.log("Service Worker registered:", registration);
+            })
+            .catch((error) => {
+                console.error("Service Worker registration failed:", error);
+            });
+    });
+}
